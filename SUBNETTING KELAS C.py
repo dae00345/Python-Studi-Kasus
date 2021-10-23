852e128b80350f6ae9247d8620fac8cbff23064e
Erick Dae <92661190+dae00345@users.noreply.github.com>
2021-10-23 17:26:38 +0800

    Add files via upload

---
+++ SUBNETTING KELAS C.py
@@ -0,0 +1,143 @@
+#Import Modul Math dan Os
+import math as m
+import os
+import terminaltables
+from time import sleep
+
+#============================= FUNCTION =================================================+
+
+#fungsi clear screen
+def cls():
+	command = 'clear'
+	if os.name in('nt','dos'):
+		command = 'cls'
+	else:
+		pass
+	os.system(command)
+
+#Fungsi Menu Awal Program
+def menu():
+	cls()
+	print("|=================================|")
+	print("|   PROGRAM SUBNETTING KELAS C    |")
+	print("|=================================|")
+	print("| >> AUTHOR ERICK DAE             |")
+	print("| >> WibuCode Community           |")
+	print("|=================================|")
+
+#fungsi menampilkan tabel prefix
+def tabelPrefix():
+	kolom = ['NO','BITS/CIDR','JUMLAH IP']
+	daftar = [(i+1,listbit[i],listhost[i])for i in range(len(listbit))]
+
+	data_table = [kolom]
+	data_table.extend(daftar)
+	table = terminaltables.AsciiTable(data_table)
+	print("\n| >> TABEL PREFIX ")
+	print(table.table)
+
+def angka(pesan):
+	while True:
+		try:
+			ipt = int(input(pesan))
+			return ipt
+		except ValueError:
+			menu()
+			tabelPrefix()
+			print("\n| [- WARNING -] : Inputan Hanya Menerima Angka")
+
+def looping(pesan):
+	ipt = input(pesan)
+	while ipt != 'y' and ipt != 't':
+		output()
+		print("\n|- WARNING -| : Inputan Hanya Menerima Y atau T saja : ")
+		ipt = input(pesan)
+	return ipt
+
+def output():
+	cls()
+	print("|=======================================================|")
+	print("| >> Diketahui :                                        |")
+	print("|-------------------------------------------------------|")
+	print("|          >> Prefix/CIDR = /",bit)
+	print("|            >> Jumlah Ip = ",jumlahip)
+	print("|-------------------------------------------------------|")
+	print("| >> Penyelesaian :                                     |")
+	print("|-------------------------------------------------------|")
+	print("|                >> Hasil = ",ipOktet4,"/",jumlahip,"=",hasil ,"=",final_hasil," [Ambil Angka Sebelum Koma]") 
+	print("|     >> Ip Awal[NETWORK] = ",final_hasil,"*",jumlahip,"=",ipawal)
+	print("| >>  Ip Akhir[BROADCAST] = ",ipawal,"+",jumlahip,"-",1,"=",ipakhir)
+	print("|            >> Host Awal = ",ipawal,"+",1,"=",hostawal)
+	print("|           >> Host Akhir = ",ipakhir,"+",1,"=",hostakhir)
+	print("|          >> Subnet Mask = ",256 ,"-", jumlahip,"=",subnet_mask)
+	print("|          >> Usable Ips  = ",hostakhir,"-",hostawal,"+",1,"=",usableIp)
+	print("|-------------------------------------------------------|")
+	print("|   >> RENTANG IP = ",ipawal," Sampai ",ipakhir)
+	print("| >> RENTANG HOST = ",hostawal,"Sampai",hostakhir)
+	print("|  >> SUBNET MASK = ",subnet_mask)
+	print("|   >> USABLE IPs = ",usableIp)
+	print("|=======================================================|")
+
+#loading screen
+def load():
+	for i in range(5,0,-1):
+		cls()
+		print("| >> Sedang Menghitung")
+		print("| >> Hitung Mundur : ",i)
+		sleep(0.2)
+
+	
+
+#=========================== END  FUNCTION =========================================+
+
+
+#=========================== MAIN ==================================================+
+#list tabel prefix
+listbit = [24,25,26,27,28,29,30,32]
+listhost = [256,128,64,32,16,8,4,1]
+ulang = 'y'
+
+while ulang == 'y':
+	menu()
+	tabelPrefix()
+
+	#inputan cidr/prefix dan ip oktet 4
+	iptPrefix = angka("| >> Masukkan CIDR : ")
+	#validasi inputan cidr/prefix hanya menerima data dalam listbit
+	while iptPrefix not in listbit:
+		cls()
+		menu()
+		tabelPrefix()
+		print("\n|- WARNING -| : CIDR Tidak Tersedia Untuk Kelas C")
+		iptPrefix = angka("| >> Masukkan CIDR : ")
+
+	#inputan IP Oktet 4
+	ipOktet4 = angka("| >> Masukkan Ip Oktet 4 : ")
+
+	#validasi inputan user
+	for i in range(len(listbit)):
+		if iptPrefix == listbit[i]:
+			bit = listbit[i]
+			host = listhost[i]
+		else:
+			pass
+
+	#menghitung
+	jumlahip = host
+	hasil = ipOktet4/jumlahip
+	final_hasil = m.floor(hasil)
+	ipawal = final_hasil*jumlahip # IP NETWORK
+	ipakhir = ipawal+jumlahip - 1 #IP BROADCAST
+	hostawal = ipawal+1
+	hostakhir = ipakhir-1
+	subnet_mask = 256-jumlahip
+	usableIp = hostakhir-hostawal+1
+
+	load()
+	output()
+	ulang = looping("| >> Ulangi Program? <Y/T> : ")
+
+print("|---------------------|")
+print("| >> PROGRAM BERAKHIR |")
+print("|---------------------|")
+#=========================== END MAIN ==============================================+
